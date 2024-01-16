688d3a06be22a450e44ba7a0514a7d9306900b57
Jahangeer <writetojahangeer@gmail.com>
2024-01-16 16:59:50 +0530

    Update tele.js

--- tele.js
+++ tele.js
@@ -5,8 +5,8 @@ const token = telegramConfig.token;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("msg", (msg) => {
-//   console.log("inside channel msg");
-//   console.log(msg);
+  console.log("inside channel msg");
+  console.log(msg);
   const chatId = msg.chat.id;
 
   const sourceChannelId = telegramConfig.sourceId;
@@ -20,8 +20,8 @@ bot.on("msg", (msg) => {
 });
 
 bot.on("channel_post", (msg) => {
-//   console.log("inside channel post");
-//   console.log(msg);
+  console.log("inside channel post");
+  console.log(msg);
 //   console.log(msg.chat);
 //   console.log(msg.chat.id);
 //   const chatId = msg.chat.id;
